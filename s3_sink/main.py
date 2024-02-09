56cd6cfd8c6ae81d97b35978556fec93b9088fe4
Tun <tun@quix.io>
2024-02-09 00:30:33 +0000

    Edited 'main.py'

--- s3_sink/main.py
+++ s3_sink/main.py
@@ -1,13 +1,22 @@
 import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+
 from quixstreams import Application, State
 from quixstreams.models.serializers.quix import JSONDeserializer, JSONSerializer
 
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id = os.environ["aws_access_key_id"],
+    aws_secret_access_key = os.environ["aws_access_key"]
+)
+
 # Create an Application.
 app = Application.Quix(
-            consumer_group="s3_sink",
-            auto_offset_reset="earliest",
-            auto_create_topics=True,  # Quix app has an option to auto create topics
-        )
+        consumer_group="s3_sink",
+        auto_offset_reset="earliest",
+        auto_create_topics=True,  # Quix app has an option to auto create topics
+    )
 
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
 
@@ -15,7 +24,22 @@ sdf = app.dataframe(input_topic)
 
 def upload_to_s3(row: dict):
 
-    print(f"********** row: {row}")
+    df = pd.DataFrame.from_dict(data, orient='index')
+    write_pandas_parquet_to_s3(
+        df, "bucket", "folder/test/file.parquet", ".tmp/file.parquet")
+
+    (df, bucketName, keyName, fileName):
+    # dummy dataframe
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, fileName)
+
+    # upload to s3
+    s3 = boto3.client("s3")
+    BucketName = bucketName
+    with open(fileName) as f:
+       object_data = f.read()
+       s3.put_object(Body=object_data, Bucket=BucketName, Key=keyName)
+
     
     return row
 
