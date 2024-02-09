9815bc441a7727d3cf0102815e8d7c82a998d833
Tun <tun@quix.io>
2024-02-09 02:28:17 +0000

    Edited 'main.py'

--- dialogue_counter_s3_sink/main.py
+++ dialogue_counter_s3_sink/main.py
@@ -33,8 +33,10 @@ def upload_to_s3(row: dict):
     
     print(f"Writing to {path}")
     # Storing data in data lake
+    data = {'name': character_name, 'count': row["count"]}
+
     wr.s3.to_parquet(
-        df=pd.DataFrame(row["count"], index=[0]),
+        df=pd.DataFrame(data, index=[0]),
         path=path,
         boto3_session=my_session
     )
