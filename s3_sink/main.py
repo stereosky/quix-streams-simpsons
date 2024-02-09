bd8fdb6b22653ce8b29528378dc69a235baa8915
Tun <tun@quix.io>
2024-02-09 00:07:33 +0000

    Edited 'main.py'

--- s3_sink/main.py
+++ s3_sink/main.py
@@ -11,8 +11,8 @@ from quixstreams import Application, State, message_key
 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=os.environ("aws_access_key_id"),
-    aws_secret_access_key=os.environ("aws_access_key")
+    aws_access_key_id=os.environ["aws_access_key_id"],
+    aws_secret_access_key=os.environ["aws_access_key"]
 )
 
 def write_pandas_parquet_to_s3(df, bucketName, keyName, fileName):
@@ -28,7 +28,7 @@ def write_pandas_parquet_to_s3(df, bucketName, keyName, fileName):
 def upload_row(row: dict):
     df = pd.DataFrame.from_dict(row, orient='index')
     write_pandas_parquet_to_s3(
-        df, os.environ("s3_bucket"), "test/file.parquet", ".tmp/file.parquet")
+        df, os.environ["s3_bucket"], "test/file.parquet", ".tmp/file.parquet")
     
     return row
 
@@ -39,7 +39,7 @@ app = Application.Quix(
 )
 
 # Define an input topic with Quix deserializer
-input_topic = app.topic(os.environ("input"), value_deserializer="json")
+input_topic = app.topic(os.environ["input"], value_deserializer="json")
 
 # Create a StreamingDataFrame and start building your processing pipeline
 sdf = app.dataframe(input_topic)
