738c741cbf0e5745a4b2a235a700b2f192a289a7
Tun <tun@quix.io>
2024-02-09 01:30:23 +0000

    Edited 'main.py'

--- s3_sink/main.py
+++ s3_sink/main.py
@@ -27,9 +27,7 @@ sdf = app.dataframe(input_topic)
 def upload_to_s3(row: dict):
 
     # df = pd.DataFrame.from_dict(row, orient='index')
-
-    # wr.s3.to_parquet(df, "s3://hackathon-quix-tun/data/df.parquet")
-    df = pd.DataFrame({"id": [1, 2], "value": ["foo", "boo"]})
+    df = pd.DataFrame(row)
 
     # Storing data on Data Lake
     wr.s3.to_parquet(
